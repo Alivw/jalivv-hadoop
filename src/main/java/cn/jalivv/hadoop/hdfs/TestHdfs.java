6183dc0119dd797a343d3587afa87f59a33a0046
jalivv <shizuwei99@gmail.com>
2022-09-03 12:19:09 +0800

    test block

--- src/main/java/cn/jalivv/hadoop/hdfs/TestHdfs.java
+++ src/main/java/cn/jalivv/hadoop/hdfs/TestHdfs.java
@@ -2,9 +2,7 @@ package cn.jalivv.hadoop.hdfs;
 
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
 import org.apache.hadoop.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -69,6 +67,33 @@ public class TestHdfs {
         }
     }
 
+
+    @Test
+    public void blocks() throws Exception {
+        Path path = new Path("/user/god/data.txt");
+        FileStatus file = fs.getFileStatus(path);
+        BlockLocation[] blockLocations = fs.getFileBlockLocations(path, 0, file.getLen());
+        for (BlockLocation location : blockLocations) {
+            System.out.println(location);
+        }
+
+        /*
+            0,      1048576,node01
+            1048576,840319,node01
+         */
+
+        /**
+         * 计算向数据移动
+         * 用户和程序读取的是文件这个级别 并不知道有块的概念
+         */
+        FSDataInputStream in = fs.open(path);
+        in.seek(1048576);
+        // 计算向数据移动后，期望的是分治，只读取自己关心（通过seek实现），同时，具备距离的概念（优先和本地的DN获取数据--框架的默认机制）
+        byte[] res = new byte[20];
+        in.read(res, 0, 20);
+        System.out.println(new String(res));
+
+    }
 
     @After
     public void close() {
